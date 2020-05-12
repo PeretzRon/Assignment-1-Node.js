dfe0d80e525712d7e89662cf8f6b8549c0a5a023
ronpe <ron.peretz1@gmail.com>
2020-05-13 00:43:11 +0300

    Add form to /users & parse data from request

--- routes.js
+++ routes.js
@@ -1,25 +1,56 @@
-
-const requestHandler = (req, res)=> {
+const requestHandler = (req, res) => {
     const url = req.url;
-    res.write('<html>');
-    res.write('<head><title>Assignment 1</title></head>');
+    const method = req.method;
+
     switch (url) {
         case '/' :
-            res.write('<body><h1>This is the first assignment in Node.js</h1></body>');
-            break;
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<html>');
+            res.write('<head><title>Assignment 1</title></head>');
+            res.write('<body><h1>This is the first assignment in Node.js</h1>' +
+                '<form action="/create-user" method="POST"><input type="text" placeholder="Enter User" name="user"><button type="submit">Send</button></form></body>');
+            res.write('</html>');
+            return res.end();
         case '/users' :
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<html>');
+            res.write('<head><title>Assignment 1</title></head>');
             res.write(
                 '<body><h1>This is the first assignment in Node.js</h1>' +
                 '<ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>'
             );
+            res.write('</html>');
+            return res.end();
+        case '/create-user':
+            if (method === 'POST') {
+                const body = [];
+                req.on('data', chunk => {
+                    body.push(chunk);
+                })
+                req.on('end', () => {
+                    const parsedBody = Buffer.concat(body).toString();
+                    const user = parsedBody.split('=')[1];
+                    console.log(user);
+                });
+                res.statusCode = 302;
+                res.setHeader('Location', '/users');
+                res.end();
+            }else {
+                wrongPathHandler(res);
+            }
             break;
         default:
-            res.write('<body><h1>Opps... Wrong path - 404</h1></body>');
-
+           wrongPathHandler(res);
     }
+}
 
+const wrongPathHandler = (res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Assignment 1</title></head>');
+    res.write('<body><h1>Oops.. Wrong path - 404 </h1>');
     res.write('</html>');
-    return res.end();
+    res.end();
 }
 
 exports.handler = requestHandler;
